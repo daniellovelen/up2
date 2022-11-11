68cdedc04c96fc0a4d8ce5772d6e9a2041e1b9ee
danula <danulaloveanume@gmail.com>
2022-11-11 09:54:19 +0300

    first commit

--- init.php
+++ init.php
@@ -5,5 +5,6 @@ $username="root";
 $password="root";
 $dbname="up2";
 
+
 $db_handler=mysqli_connect($hostname,$username, $password) or die ("Не удается подключиться к базе данных!");
 mysqli_select_db($db_handler,$dbname) or die ("Ошибка выбора базы данных!");
